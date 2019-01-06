de2e27550e52bd172e562a1c8f34063e3accfae9
Jiang Wenbo <jian0085@e.ntu.edu.sg>
2019-01-06 10:57:05 +0800

    Update DecodeQueue.hpp

--- include/DecodeQueue.hpp
+++ include/DecodeQueue.hpp
@@ -37,39 +37,39 @@ public:
 	}
     // if queue is full, discard the incoming data
     // if push data fail, return -1
-    int push(T t)
-    {
-        int size;
-        sem_getvalue(&sem, &size);
-        // Keep waiting for free capacities
-        do
-        {
-            sem_getvalue(&sem, &size);
-        } while (size >= _size);
-        //if(size==_size) return -1;
-        _data[_head] = t;
-        _head = (_head + 1) % _size;
-        sem_post(&sem);
+	int push(T t)
+	{
+		int size;
+		sem_getvalue(&sem, &size);
+		// Keep waiting for free capacities
+		do
+		{
+			sem_getvalue(&sem, &size);
+		} while (size >= _size);
+		//if(size==_size) return -1;
+		_data[_head] = t;
+		_head = (_head + 1) % _size;
+		sem_post(&sem);
 	}
     // if queue is empty, wait
-    T pop()
-    {
-        sem_wait(&sem);
-        T t = _data[_end];
-        _end = (_end + 1) % _size;
-        return t;
-    }
+	T pop()
+	{
+		sem_wait(&sem);
+		T t = _data[_end];
+		_end = (_end + 1) % _size;
+		return t;
+	}
 
     // get queue size
-    int get_size()
-    {
-        /*
+	int get_size()
+	{
+		/*
 		int size = 0;
 		sem_getvalue(&sem, &size);
 		return size;
 		*/
-        return (_head - _end) % _size;
-    }
+		return (_head - _end) % _size;
+	}
 };
 
 #endif
